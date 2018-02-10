8f72e267882de2b25d5c0d6ceb1a3758f72990b6
Kumaran Rajendhiran <kumaranvpl@gmail.com>
2018-02-10 10:05:02 +0530

    Update logic to reflect site changes and Move to python3

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from  bs4 import BeautifulSoup
 from tqdm import tqdm
-from urlparse import urlparse
+from urllib.parse import urlparse
 
 import fire
 import os
 import re
 import requests
-import urllib2
-import urllib
 
 
 def crawl_tamilanda(album_url, save_dir=False):
@@ -22,8 +20,8 @@ def crawl_tamilanda(album_url, save_dir=False):
     album_name = False
     song_urls = []
 
-    for li in soup.find_all(class_="collection-item avatar"):
-        file_url = li.a.get('href').replace("filepc", "download")
+    for li in soup.find_all("small"):
+        file_url = li.a.get('href').replace("file.php", "download.php")
 
         if not album_name:
             album_name = file_url.split("/")[-2]
@@ -34,11 +32,11 @@ def crawl_tamilanda(album_url, save_dir=False):
 
     album_dir = save_dir+"/"+album_name
 
-    print "Found " + `len(song_urls)` + " songs"
+    print("Found " + str(len(song_urls)) + " songs")
     count = 0
     for song_url in song_urls:
         count += 1
-        print `count` + ". " + song_url.split(".flac")[0].split("/")[-1]
+        print(str(count) + ". " + song_url.split(".flac")[0].split("/")[-1])
 
     hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -55,27 +53,6 @@ def crawl_tamilanda(album_url, save_dir=False):
         wget_command = "wget -c '" + song_url + "' -O " + "'" + song_path + "'"
 
         os.system(wget_command)
-        """req = urllib2.Request(song_url, headers=hdr)
-        u = urllib2.urlopen(req)
-        f = open(album_dir+"/"+song_name, 'wb')
-        meta = u.info()
-        file_size = int(meta.getheaders("Content-Length")[0])
-        print "Downloading: %s Bytes: %s" % (song_name, file_size)
-
-        file_size_dl = 0
-        block_sz = 8192
-        while True:
-            buff = u.read(block_sz)
-            if not buff:
-                break
-
-            file_size_dl += len(buff)
-            f.write(buff)
-            status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-            status = status + chr(8)*(len(status)+1)
-            print status,
-
-        f.close()"""
 
 
 def main():
